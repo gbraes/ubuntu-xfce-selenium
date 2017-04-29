9bab5926d2151ecc8c29bfb7840522109df0f2b9
german <gbravoescobar@gmail.com>
2017-04-29 16:55:24 +0200

    Deleted attempt to change terminal font size

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@ if [ ! -f /.root_pw_set ]; then
 	/set_root_pw.sh
 fi
 
-#Change terminal size
-sed -i "s/FontName=Monospace.*/FontName=Monospace 10/g" ~/.config/xfce4/terminal/terminalrc
 
 dbus-daemon --system --fork
 
